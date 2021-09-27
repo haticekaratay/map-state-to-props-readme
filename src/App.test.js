5e8ab5447e65d038761a27a99d7f0096fd35955d
Hatice <haticekaraty@gmail.com>
2021-09-27 14:25:06 -0400

    Done.

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { createStore } from 'redux';
+import { Provider } from "react-redux";
+import counterReducer from './reducers/counterReducer.js';
+
+const store = createStore(
+  counterReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+); 
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider >, div);
 });
